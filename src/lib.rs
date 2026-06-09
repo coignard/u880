08ce6bdbbd2bb86e854126a876ba7185f0b3cda8
René Coignard <contact@renecoignard.com>
2026-06-09 21:35:12 +0200

    chore(lib): update description

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! A cycle-stepped, dependency-free MME U880 emulator.
+//! A cycle-stepped MME U880 emulator.
 //!
 //! # Example
 //!
